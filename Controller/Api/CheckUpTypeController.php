a5d305ab0ae562eeec6fe733011a513d3f628fdd
PhamToanXuan <phamxuantoan2710@gmail.com>
2023-11-06 20:39:18 +0700

    Config Database php and Demo

---
+++ Controller/Api/CheckUpTypeController.php
@@ -0,0 +1,55 @@
+<?php
+
+
+class CheckUpType extends BaseController
+{
+
+    public function listType ()
+    {
+        $strErrorDesc = '';
+        $requestMethod = $_SERVER["REQUEST_METHOD"];
+        $arrQueryStringParams = $this->getQueryStringParams();
+        if (strtoupper($requestMethod) == 'GET') {
+            try {
+                $userModel = new UserModel();
+                $intLimit = 10;
+                if (isset($arrQueryStringParams['limit']) && $arrQueryStringParams['limit']) {
+                    $intLimit = $arrQueryStringParams['limit'];
+                }
+                $arrUsers = $userModel->getCheckUpType($intLimit);
+                $responseData = json_encode($arrUsers);
+            } catch (Error $e) {
+                $strErrorDesc = $e->getMessage().'Something went wrong! Please contact support.';
+                $strErrorHeader = 'HTTP/1.1 500 Internal Server Error';
+            }
+        } else {
+            $strErrorDesc = 'Method not supported';
+            $strErrorHeader = 'HTTP/1.1 422 Unprocessable Entity';
+        }
+        // send output 
+        if (!$strErrorDesc) {
+            $this->sendOutput(
+                $responseData,
+                array('Content-Type: application/json', 'HTTP/1.1 200 OK')
+            );
+        } else {
+            $this->sendOutput(json_encode(array('error' => $strErrorDesc)), 
+                array('Content-Type: application/json', $strErrorHeader)
+            );
+        }
+    }
+    // private $db;
+    // public function __construct()
+    // {
+    //     $this->db = (new Database())->getConnection();
+    // }
+    // public function getData()
+    // {
+    //     // Your code for creating a record in 'cats' table
+    //     $query = "SELECT * FROM checkuptype";
+    //     $stmt = $this->db->prepare($query);
+    //     $stmt->execute();
+    //     return $stmt->fetchAll(PDO::FETCH_ASSOC);
+    // }
+}
+?>
